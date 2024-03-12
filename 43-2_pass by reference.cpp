24525986d40b7dd4f2fc50b6a8460d5271b62ec9
フユカワ(Fuyukawa) <54656519+hyonestar19@users.noreply.github.com>
2024-03-13 03:37:23 +0900

    Create 43-2_pass by reference.cpp

---
+++ 43-2_pass by reference.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(string &x, string &y);
+
+int main()
+{
+	string x = "Kool-Aid";
+	string y = "Water";
+
+
+	swap(x,y);
+
+	cout << "X: " << x << endl;
+	cout << "Y: " << y << endl;
+
+	return 0;
+}
+
+void swap(string &x, string &y)
+{
+	string temp;
+	temp = x;
+	x = y;
+	y = temp;
+}
