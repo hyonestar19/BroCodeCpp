353271f77eb0c63e52bbf8e165a764cbe09f4035
フユカワ(Fuyukawa) <54656519+hyonestar19@users.noreply.github.com>
2024-03-13 02:59:29 +0900

    Create 01_Variables and basic data types.cpp

---
+++ 01_Variables and basic data types.cpp
@@ -0,0 +1,54 @@
+// Variable
+int x; // 선언
+x = 5; // 할당
+
+int x = 5; // 선언 후 할당
+
+// Integer
+int age = 23;
+int year = 2023;
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int days = 7.5;
+	cout << days << endl;
+	return 0;
+}
+
+// Double
+double price = 10.99;
+double gpa = 2.5;
+double temperature = 25;
+cout << temperature << endl;
+
+// Single Character
+char grade = 'A';
+char initial = 'B';
+char currency = '$';
+char myChar = 125; // ASCII
+
+// Boolean
+bool student = false;
+bool power = true;
+bool forSale = true;
+
+// String
+// In C++
+string name = "Lee";
+string day = "Friday";
+string food = "pizza";
+string address = "123 Fake St.";
+
+cout << "Hello " << name << endl;
+
+// In C
+char name [] = "Lee";
+char day [] = "Friday";
+char food [] = "pizza";
+char address [] = "123 Fake St.";
+
+printf("Hello %s \n", name);
