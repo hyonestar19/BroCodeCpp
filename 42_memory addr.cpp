706e6c7d4cfd87dff818b2324ccf7201c0e6a8a2
フユカワ(Fuyukawa) <54656519+hyonestar19@users.noreply.github.com>
2024-03-13 03:36:29 +0900

    Create 42_memory addr.cpp

---
+++ 42_memory addr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string name = "Bro";
+	int age = 21;
+	bool student = true;
+
+	cout << &name << endl;
+	cout << &age << endl;
+	cout << &student << endl;
+	
+	return 0;
+}
