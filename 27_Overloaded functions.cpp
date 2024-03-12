de7a0ae022ec19da7233d833ca18ca7c31b7d955
フユカワ(Fuyukawa) <54656519+hyonestar19@users.noreply.github.com>
2024-03-13 03:26:21 +0900

    Create 27_Overloaded functions.cpp

---
+++ 27_Overloaded functions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void bakePizza();
+void bakePizza(string topping1);
+void bakePizza(string topping1, string topping2);
+
+int main()
+{
+	//bakePizza();
+	//bakePizza("pepperoni");
+	//bakePizza("pepperoni", "mushroom");
+	return 0;
+}
+
+void bakePizza() {
+	cout << "Here is your pizza!\n";
+}
+
+void bakePizza(string topping1)
+{
+	cout << "Here is your " << topping1 << " pizza!\n";
+}
+
+void bakePizza(string topping1, string topping2)
+{
+	cout << "Here is your " << topping1 << "and" << topping2 << " pizza!\n";
+}
