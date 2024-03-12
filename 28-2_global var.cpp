add5fa6a95ec5e93492e727e4d83b7235224dff5
フユカワ(Fuyukawa) <54656519+hyonestar19@users.noreply.github.com>
2024-03-13 03:29:46 +0900

    Create 28-2_global var.cpp

---
+++ 28-2_global var.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int myNum = 3;
+
+void printNum();
+
+int main()
+{
+	int myNum = 1;
+	printNum();
+	cout << myNum << endl;
+
+	return 0;
+}
+
+void printNum()
+{
+	int myNum = 2;
+	cout << myNum << endl;
+}
