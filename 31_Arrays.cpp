5ddc5ced3b287a166013edc7a83a05998e21104d
フユカワ(Fuyukawa) <54656519+hyonestar19@users.noreply.github.com>
2024-03-13 03:31:20 +0900

    Create 31_Arrays.cpp

---
+++ 31_Arrays.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string car[] = {"Corvette", "Mustang", "Camry"};
+	cout << car[0] << endl; // Corvette이 출력된다.
+	cout << car[1] << endl; // Mustang이 출력된다.
+	cout << car[2] << endl; // Camry가 출력된다.
+
+	return 0;
+}
